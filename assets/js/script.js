5c085fc3428dd0f748d839cf833f18ebb5c75188
Terry Damasio <terrydamasio.dev@gmail.com>
2023-10-20 10:58:48 -0300

    atualização projeto

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+function nextAboutUs(){
+    document.getElementById("about-us-text").innerHTML = `Equipe:<br><br>
+    Gabriel Calado 0161144 S.I<br>
+    Gabriel César Ferreira Guimarães 01589747 E.C<br>
+    Terry Damasio Santos 01598110 C.C<br>
+    Rodrigo Andrade Cavalcante Muniz 01606059 C.C<br>
+    Marcus Vinicius Ferraz Teixeira de Mendonça 01545162 S.I<br>
+    João Gabriel Matias Souza 01603557 E.C<br>
+    Eliel Lucas Trajano Neto 01606048 C.C<br>
+    David Tavares de Oliveira Simões 01590461 C.C<br>
+    Brenno França Malafaia Barboza de Castro 01591145 C.C<br>
+    Danilo Mateus Silva Do Nascimento 01615380 S.I`
+
+    document.getElementById("next-page1").innerHTML = "<a style='text-decoration: none; color: white; margin-top: 145px' class='next-page' href='sobrenos.html'><</a>"
+
+}
+
+function nextProject(){
+    document.getElementById("change-text-project").innerHTML = ""
+    document.getElementById("project-text").innerHTML = `<p style='font-size: 17px;'>Simplicidade Elegante: Criccio ToDo oferece uma interface limpa e intuitiva, tornando a gestão de tarefas uma experiência agradável.<br>Você não precisa ser um expert em tecnologia para usá-lo efetivamente.<br><br>
+    Organização Impecável: Crie listas de tarefas, atribua prioridades, defina prazos e mantenha tudo sob controle. Com recursos de arrastar e soltar, você pode reorganizar suas tarefas em segundos.<br><br>
+    Colaboração Eficiente: Compartilhe listas de tarefas com colegas de trabalho, familiares ou amigos. Trabalhe em equipe de forma mais eficaz e mantenha todos na mesma página.<br><br>
+    Lembretes Inteligentes: Receba lembretes personalizáveis para garantir que nada seja esquecido. Nunca mais perca prazos importantes ou eventos essenciais.<br><br>
+    Sincronização em Tempo Real: Acesse suas tarefas em qualquer lugar, a qualquer hora, em todos os seus dispositivos. Sincronização instantânea para manter você atualizado, seja no celular, tablet ou desktop.<br><br>
+    Estatísticas de Desempenho: Obtenha insights sobre sua produtividade e descubra onde você pode melhorar. Criccio ToDo oferece estatísticas detalhadas para que você possa tomar decisões.<br>
+    </p>
+    <a style='font-size: 45px; margin-top:-10px; text-decoration: none; color:white;' href='projeto.html'><</a>
+    `
+
+}
